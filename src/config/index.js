7992afa145a57ab6af193501258d631a99194150
Mahiler <fernando.mahiler@gmail.com>
2021-01-19 00:12:13 -0500

    [UPDATE]: Scripts with package.json

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line global-require
+    require('dotenv').config();
+}
+
+console.log(process.env);
 module.exports = {
     API_KEY_GIF: process.env.REACT_APP_API_KEY,
+    PUBLIC_URL: process.env.REACT_APP_PUBLIC_URL,
 };
